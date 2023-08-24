b51dc8c85af55cbcf0859294a77c961ad491c95a
antonythomas123 <antonythomas993@gmail.com>
2023-08-24 17:07:49 +0530

    Login Page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />}/>
+          <Route path="/home" element={<Home/>}/>
+        </Routes> 
+      </BrowserRouter>
     </div>
   );
 }
