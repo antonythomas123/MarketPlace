138227771536a5651031a0bce3752f796c4f5525
antonythomas123 <antonythomas993@gmail.com>
2023-08-27 15:28:55 +0530

    Added Review Posting

--- src/pages/ProductPage/ProductPage.js
+++ src/pages/ProductPage/ProductPage.js
@@ -8,15 +8,26 @@ import { useLocation } from "react-router-dom";
 import Slider from "../../components/Slider/Slider";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import FlashOnIcon from "@mui/icons-material/FlashOn";
-import { Rating } from "@mui/material";
+import { Rating, TextField } from "@mui/material";
 import CustomAppBar from "../../components/CustomAppBar/CustomAppBar";
 import { useStateValue } from "../../contexts/StateProvider";
 import { useUserContext } from "../../contexts/UserContext";
+import { getUsersReviewCollection } from "../../services/database";
+import { async } from "q";
 
 const defaultTheme = createTheme();
 
 export default function ProductPage() {
+  const [openReview, setOpenReview] = React.useState(false);
+  const [review, setReview] = React.useState(0);
+  const [reviewStatement, setReviewStatement] = React.useState("");
   const location = useLocation();
+  const [userReviews, setUserReviews] = React.useState({
+    rating: 0,
+    reviewStatement: "",
+  });
+
+  const [usersReviews, setUsersReviews] = React.useState([]);
 
   const {
     brand,
@@ -32,7 +43,7 @@ export default function ProductPage() {
   } = location.state;
   const [basket, dispatch] = useStateValue();
 
-  const {user}  = useUserContext();
+  const { user } = useUserContext();
 
   const addToCart = () => {
     dispatch({
@@ -47,13 +58,43 @@ export default function ProductPage() {
         rating: rating,
         discountPercentage: discountPercentage,
       },
-      email: user.email
+      email: user.email,
     });
   };
+  React.useEffect(() => {
+    getUsersReviewCollection().then((loadedReviews) => {
+      setUsersReviews([...loadedReviews.data]);
+    });
+  }, []);
 
+  const handleRateProduct = () => {
+    setOpenReview(true);
+  };
+
+  const handleReviewSubmit = async() => {
+    setUserReviews({
+      ...userReviews,
+      rating: review,
+      reviewStatement: reviewStatement,
+    });
+    const reviews = await getUsersReviewCollection();
+
+    const newReview = {
+      productId: id,
+      userId: user.email,
+      rating: review,
+      reviewStatement: reviewStatement,
+    };
+    if (reviews) {
+      reviews.insert(newReview);
+      setOpenReview(false);
+    }else{
+      console.log("review error")
+    }
+  };
   return (
     <ThemeProvider theme={defaultTheme}>
-      <CustomAppBar/>
+      <CustomAppBar />
       <Grid container sx={{ height: "100%" }}>
         <Grid item sx={{ width: "50%" }}>
           <Grid
@@ -220,12 +261,111 @@ export default function ProductPage() {
                   </Typography>
                   <Rating name="read-only" value={rating} readOnly />
                 </Grid>
-                <Grid item margin="10px">
-                  <Paper elevation={2}>
-                    <Button>Rate Product</Button>
-                  </Paper>
-                </Grid>
+                {openReview ? (
+                  ""
+                ) : (
+                  <Grid item margin="10px">
+                    <Paper elevation={2}>
+                      <Button onClick={() => handleRateProduct()}>
+                        Rate Product
+                      </Button>
+                    </Paper>
+                  </Grid>
+                )}
+                {openReview && (
+                  <Grid container flexDirection="column" p={2}>
+                    <Grid item sx={{ borderBottom: "1px solid #999", mb: 3 }}>
+                      <Typography>Rate this Product</Typography>
+                      <Typography>
+                        <Rating
+                          name="read-only"
+                          value={review}
+                          onChange={(event, newValue) => {
+                            setReview(newValue);
+                          }}
+                        />
+                      </Typography>
+                    </Grid>
+                    <Grid item>
+                      <Typography>Review This Product</Typography>
+                      <TextField
+                        id="filled-multiline-static"
+                        label="Description"
+                        multiline
+                        rows={4}
+                        variant="filled"
+                        sx={{ width: "100%", mb: 3, background: "white" }}
+                        onChange={(e) => setReviewStatement(e.target.value)}
+                      />
+                    </Grid>
+                    <Grid container justifyContent="flex-end">
+                      <Grid item>
+                        <Button
+                          variant="outlined"
+                          sx={{
+                            marginRight: "10px",
+                            background: "#ff9f00",
+                            color: "white",
+                            borderColor: "1px solid #ff9f00",
+                          }}
+                          onClick={handleReviewSubmit}
+                        >
+                          SUBMIT
+                        </Button>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                )}
               </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid container justifyContent="center" sx={{ mt: 3 }}>
+            <Grid
+              item
+              xs={10}
+              component={Paper}
+              elevation={6}
+              square
+              borderRadius="10px"
+            >
+              {userReviews.rating > 0 && (
+                <Grid
+                  container
+                  flexDirection="column"
+                  p={2}
+                  mt={2}
+                  sx={{ borderBottom: "1px solid #999" }}
+                >
+                  <Typography>Your Review</Typography>
+                  <Rating
+                    name="read-only-user"
+                    value={userReviews.rating}
+                    readOnly
+                  />
+                  <Typography>{userReviews.reviewStatement}</Typography>
+                </Grid>
+              )}
+
+              {usersReviews?.map((reviewItem, index) => (
+                <Grid
+                  key={index}
+                  container
+                  flexDirection="column"
+                  p={2}
+                  mt={2}
+                  sx={{ borderBottom: "1px solid #999" }}
+                >
+                  <Typography>User Review</Typography>
+                  <Typography fontWeight="bold">{reviewItem.userId}</Typography>
+                  <Rating
+                    name={`read-only-${index}`}
+                    value={reviewItem.rating}
+                    readOnly
+                  />
+                  <Typography>{reviewItem.reviewStatement}</Typography>
+                </Grid>
+              ))}
             </Grid>
           </Grid>
         </Grid>
--- src/services/database.js
+++ src/services/database.js
@@ -1,6 +1,6 @@
-import Loki from 'lokijs';
+import Loki from "lokijs";
 
-const db = new Loki('auth.db', {
+const db = new Loki("auth.db", {
   autoload: true,
   autoloadCallback: databaseInitialize,
   autosave: true,
@@ -8,14 +8,33 @@ const db = new Loki('auth.db', {
 });
 
 function databaseInitialize() {
-  const users = db.getCollection('users');
+  const users = db.getCollection("users");
+ // const reviews = db.getCollection("reviews");
+
   if (users === null) {
-    db.addCollection('users', { indices: ['username'] });
+    db.addCollection("users", { indices: ["username"] });
   }
+  // if (reviews === null) {
+  //   db.addCollection("reviews", { indices: ["productId"] });
+  // }
 }
 
 export function getUsersCollection() {
-  return db.getCollection('users');
+  return db.getCollection("users");
 }
+
+export async function getUsersReviewCollection() {
+  return new Promise((resolve, reject) => {
+    db.loadDatabase({}, () => {
+      const reviews = db.getCollection("reviews");
+      if (!reviews) {
+        resolve(db.addCollection("reviews", { indices: ["productId", "userId"] }));
+      } else {
+        resolve(reviews);
+      }
+    });
+  });
+}
+
 
 export default db;
